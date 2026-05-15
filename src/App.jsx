e7cd900f799c280377ad73bd01701d237245ff4b
pizzy37 <YOUR_GITHUB_EMAIL_HERE>
2026-05-15 23:38:11 +0100

    Fix router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Profile from './pages/Profile'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/"      element={<Home />} />
         <Route path="/login" element={<Login />} />
